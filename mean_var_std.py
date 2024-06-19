4561c25bc0944fb58d2d913328d81b234440dd9a
Mohammad Ahmed Javed <aj483267@gmail.com>
2024-06-19 09:16:54 +0000

    Freecodecamp Project One

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
-    return calculations
+  if len(list) != 9:
+    raise ValueError("List must contain nine numbers.")
+  matrix = np.array(list).reshape(3, 3)
+  calculations = {
+        'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().item()],
+        'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().item()],
+        'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std().item()],
+        'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max().item()],
+        'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min().item()],
+        'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().item()]
+    }
+  return calculations
